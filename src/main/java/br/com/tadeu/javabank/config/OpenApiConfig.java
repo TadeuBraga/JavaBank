54a9c3a29f5eabb7aaf1160c8ccd1cfc4dd69750
Tadeu Braga <tadeufala@gmail.com>
2021-05-11 21:57:55 -0300

    Basic swagger configured. closes #3

---
+++ src/main/java/br/com/tadeu/javabank/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package br.com.tadeu.javabank.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().components(new Components())
+				.info(new Info().title("API de Banco Simples").description("API provê operações bancárias simples."));
+	}
+}
