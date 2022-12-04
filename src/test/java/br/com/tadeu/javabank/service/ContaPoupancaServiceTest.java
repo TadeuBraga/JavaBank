daa582f54425690f55f45df23f36e1e60324f186
Tadeu Braga <tadeufala@gmail.com>
2022-12-04 17:49:39 -0300

    Implement test for ContaPoupancaService.

---
+++ src/test/java/br/com/tadeu/javabank/service/ContaPoupancaServiceTest.java
@@ -0,0 +1,62 @@
+package br.com.tadeu.javabank.service;
+
+import br.com.tadeu.javabank.model.Banco;
+import br.com.tadeu.javabank.model.Cliente;
+import br.com.tadeu.javabank.model.ContaPoupanca;
+import br.com.tadeu.javabank.repository.ContaRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ContaPoupancaServiceTest {
+
+    public static final ContaPoupanca CONTA_POUPANCA = ContaPoupanca.builder()
+            .cliente(Cliente.builder().build()).banco(Banco.builder().build()).build();
+    public static final Instant NOW = Instant.now();
+    @Mock
+    private ContaRepository contaRepository;
+    @Mock
+    private ClienteService clienteService;
+    @Mock
+    private BancoService bancoService;
+    @Mock
+    private Clock clock;
+
+    @InjectMocks
+    private ContaPoupancaService contaPoupancaService;
+
+    @BeforeEach
+    void setUp() {
+        CONTA_POUPANCA.setDataCriacao(null);
+        when(clock.instant()).thenReturn(NOW);
+        when(clock.getZone()).thenReturn(ZoneId.of("UTC"));
+    }
+
+    void mockServices() {
+        when(clienteService.salvaOuObtemCliente(any())).thenReturn(CONTA_POUPANCA.getCliente());
+        when(bancoService.salvaOuObtemBanco(any())).thenReturn(CONTA_POUPANCA.getBanco());
+    }
+
+    @Test
+    void deve_Salvar_Quando_Chamado() {
+        mockServices();
+        contaPoupancaService.salvar(CONTA_POUPANCA);
+        assertAll(() -> verify(contaRepository).save(CONTA_POUPANCA),
+                () -> verify(clienteService).salvaOuObtemCliente(CONTA_POUPANCA.getCliente()),
+                () -> verify(bancoService).salvaOuObtemBanco(CONTA_POUPANCA.getBanco()),
+                () -> assertThat(CONTA_POUPANCA.getDataCriacao(), equalTo(LocalDateTime.ofInstant(NOW, ZoneOffset.UTC))));
+    }
+}
